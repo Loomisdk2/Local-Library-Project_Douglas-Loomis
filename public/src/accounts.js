8e45ecacbc214c46dd9666be5aa007cc062a4004
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-05-14 12:17:11 -0500

    Solved sortAccountsByLastName

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -6,10 +6,15 @@ function findAccountById(accounts, id) {
 }
 
 function sortAccountsByLastName(accounts) {
-  
+  //for (let i = 0, i < accounts.length; i++){} <---- Don't need this. .sort() does that automatcally.
+  //const lastName = accounts.name.last; <--- This causes a problem.
+  return accounts.sort((lastNameA, lastNameB) => (lastNameA.name.last.toLowerCase() > lastNameB.name.last.toLowerCase() ? 1 : -1));
+ //return accounts; <---- I can return directly.
 }
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  
+}
 
 function getBooksPossessedByAccount(account, books, authors) {}
 
