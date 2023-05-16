5a71b88a6a71098854b7bbf3fdf1d045c611711c
Douglas Loomis <loomisdk2@gmail.com>
2023-05-16 09:55:00 -0500

    Numbered Functions

--- public/src/books.js
+++ public/src/books.js
@@ -1,17 +1,20 @@
 let findAnyById = require("./helperFunctions")
 
+//////////      5        /////////////
 function findAuthorById(authors, id) {
   return author = authors.find((found) => found.id === id);
 }
 
+//////         6           ///////////////
 function findBookById(books, id) {
   return book = books.find((found) => found.id === id);
 }
-
+////////      7       //////////
 function partitionBooksByBorrowedStatus(books) {
 
 }
 
+/////////   8      /////////////
 function getBorrowersForBook(book, accounts) {}
 //hard
 module.exports = {
--- public/src/home.js
+++ public/src/home.js
@@ -1,21 +1,28 @@
 let findAnyById = require("./helperFunctions")
 
+/////     9     ////////
 function getTotalBooksCount(books) {
   return books.length;
 }
 
+//////     10    ///////
 function getTotalAccountsCount(accounts) {
   return accounts.length;
 }
 
+///////     11     /////////
 function getBooksBorrowedCount(books) {}
 
+///////    12      /////////
 function getMostCommonGenres(books) {}
 // hard
+
+//////     13     ///////////
 function getMostPopularBooks(books) {}
 
+/////     14      /////////
 function getMostPopularAuthors(books, authors) {}
-// hard (look up the .slice() use if only returning a certain number of variables.)
+// hard (look up the .slice() (use slice if only returning a certain number of variables).)
 module.exports = {
   getTotalBooksCount,
   getTotalAccountsCount,
