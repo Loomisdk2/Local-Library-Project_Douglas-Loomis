511b1a7fe0f68f056f5ccb663de9b5ddf0bd3ccd
Douglas Loomis <loomisdk2@gmail.com>
2023-05-21 09:00:45 -0500

    Found solutions for home.js

--- public/src/home.js
+++ public/src/home.js
@@ -26,19 +26,58 @@ return checkedOutTotal;
     }
   
 
-///////    12      /////////
+///////    12    passed     /////////
 function getMostCommonGenres(books) {
-  
+  let count = {};
+  books.forEach((book) => {
+    const genre = book.genre;
+    count[genre] = count[genre] ? count[genre] + 1 : 1;
+  });
+const genreCounts = Object.entries(count);
+genreCounts.sort((a, b) => b[1] - a[1]);
+const topGenres = genreCounts.slice(0, 5);
+  const result = topGenres.map(([genre, count]) => ({ name: genre, count}));
+  return result;
 }
-// hard
 
-//////     13     ///////////
+//////     13   passed  ///////////
 function getMostPopularBooks(books) {
+  const bookCounts = books.reduce((counts, book) => {
+    const borrowCount = book.borrows.length;
+    counts.push({ name: book.title, count: borrowCount });
+    return counts;
+  }, []);
 
+  const popularBooks = bookCounts
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 5);
+
+  return popularBooks;
 }
 
 /////     14      /////////
-function getMostPopularAuthors(books, authors) {}
+function getMostPopularAuthors(books, authors) {
+  const authorCounts = {};
+
+  books.forEach((book) => {
+    const { authorId, borrows } = book;
+    if (!authorCounts[authorId]) {
+      authorCounts[authorId] = 0;
+    }
+    authorCounts[authorId] += borrows.length;
+  });
+
+  const popularAuthors = Object.entries(authorCounts)
+    .map(([authorId, count]) => {
+      const { first, last } = authors.find((author) => author.id === parseInt(authorId, 10)).name;
+      return { name: `${first} ${last}`, count };
+    })
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 5);
+
+  return popularAuthors;
+}
+
 // hard (look up the .slice() (use slice if only returning a certain number of variables).)
 
 module.exports = {
